1933cac41ed7788d11dcabffce2a5d87cf4735b0
Capipocavara <helge.backhaus+capipocavara@gmail.com>
2026-04-26 21:10:35 +0200

    Fixed typo

--- Runtime/Extensions/CapyExtensions.cs
+++ Runtime/Extensions/CapyExtensions.cs
@@ -3,7 +3,7 @@ using Random = UnityEngine.Random;
 
 namespace Capybutler.Extensions
 {
-    public static class CapiExtensions
+    public static class CapyExtensions
     {
         public static float RandomFromRange(this Vector2 range) => Random.Range(range.x, range.y);
         public static int RandomFromRange(this Vector2Int range) => Random.Range(range.x, range.y);
